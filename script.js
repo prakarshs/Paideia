5279c36afc074098befb757a43a540b930468665
Prakarsh <jhethia@gmail.com>
2024-02-01 15:48:04 +0530

    scroll trigger

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var burger = document.querySelector(".ri-menu-5-fill");
+    var burger = document.querySelector(".ri-menu-4-fill");
     burger.addEventListener('click', function () {
         console.log("clicked");
-        burger.classList.toggle('ri-menu-unfold-fill');
+        burger.classList.toggle('ri-menu-unfold-line');
+        var nav = document.querySelector("header ul");
+        console.log("prakat");
+        nav.classList.toggle('nav-toggle');
     })
-
 });
+
+gsap.to("#main",{
+    backgroundColor:"#262525",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        start:"top -10%", 
+        end:"top -85%",
+        scrub : 2 
+    }
+})
